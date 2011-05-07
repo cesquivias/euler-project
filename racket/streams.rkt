e8e84a7fa2e6001fc79bb8832175e0b2ce27ce70
Cristian Esquivias <cristian.esquivias@gmail.com>
2011-05-07 15:08:36 -0700

    Added stream-take.

--- racket/streams.rkt
+++ racket/streams.rkt
@@ -1,10 +1,20 @@
 #lang racket
 
-(provide stream-nth)
+(provide stream-nth stream-take)
 
 (define (stream-nth st n)
-    (let loop ([i n]
-               [s st])
-      (if (= i 0)
-          (stream-first s)
-          (loop (- i 1) (stream-rest s)))))
+  (let loop ([i n]
+             [s st])
+    (if (= i 0)
+        (stream-first s)
+        (loop (- i 1) (stream-rest s)))))
+
+(define (stream-take st n)
+  (reverse
+   (let loop ([i n]
+              [ls '()]
+              [s st])
+     (if (= i 0)
+         ls
+         (loop (sub1 i) (cons (stream-first s) ls)
+               (stream-rest s))))))
