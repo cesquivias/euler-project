1a18a8e219b3ea8e69a547e9e60b0e1c01fc4b41
Cristian Esquivias <cristian.esquivias@gmail.com>
2012-07-28 19:51:40 -0700

    Updated formatting for problem 1 solution in haskell.

--- haskell/prob1.hs
+++ haskell/prob1.hs
@@ -1,4 +1,4 @@
-prob1 limit = sum [ i | i <- [1..limit-1], (i `mod` 3 == 0) || (i `mod` 5 == 0) ]
+prob1 limit = sum [ i | i <- [1..limit-1], i `mod` 3 == 0 || i `mod` 5 == 0 ]
 
 main = do
      print (prob1 1000)
